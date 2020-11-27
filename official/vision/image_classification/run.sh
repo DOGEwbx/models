bd7feb3fb8f9db19efcbe458cd2d553df1aecdc8
Bingxuan Wang (FA Talent) <v-biwan1@microsoft.com>
2020-11-27 17:02:42 +0800

    fix

--- official/vision/image_classification/run.sh
+++ official/vision/image_classification/run.sh
@@ -9,3 +9,5 @@ else
         taskset -c $CPU_ID python3 simulator.py --mode=train_and_eval --model_type=resnet   --dataset=imagenet --model_dir=/model_dir/ --data_dir=/data --config_file=configs/examples/resnet/imagenet/gpu.yaml --params_override='{train_dataset: {dtype: 'float16', num_examples: 11530503}, validation_dataset: {dtype: 'float16', num_examples: 450000}}'
     else
         taskset -c $CPU_ID python3 simulator.py --mode=train_and_eval --model_type=resnet   --dataset=imagenet --model_dir=/model_dir/ --data_dir=/data --config_file=configs/examples/resnet/imagenet/gpu.yaml --params_override='{train_dataset: {dtype: 'float16', num_examples: 5124668}, validation_dataset: {dtype: 'float16', num_examples: 200000}}'
+    fi
+fi
